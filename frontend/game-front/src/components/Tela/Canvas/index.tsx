b11aeef6c0b4cf336fa4daf7423a7074d0005167
YagoPassos <yp.anjos@hotmail.com>
2021-03-11 22:05:33 -0300

    Att

--- frontend/game-front/src/components/Tela/Canvas/index.tsx
+++ frontend/game-front/src/components/Tela/Canvas/index.tsx
@@ -1,20 +1,54 @@
-import { ReactNode, useRef, useEffect } from 'react';
+import { ReactNode, useRef, useEffect, useState } from 'react';
+import { CanvasContainer } from './styles';
 
 
 interface CanvasProps {
   // children: ReactNode;
 }
 
-function Canvas({  }: CanvasProps) {
+function Canvas({ }: CanvasProps) {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  useEffect(() => {
+  var context : any;
 
+  useEffect(() => {
     if (canvasRef.current) {
-   
-      const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
       
+      const canvas = canvasRef.current;
+      context = canvas.getContext('2d');
+
+      const game: any = {
+        players: {
+          'Bob': { x: 1, y: 1 },
+          'Paul': { x: 8, y: 8 }
+        },
+
+        fruits: {
+          'fruits': { x: 3, y: 1 }
+        }
+      }
+
+      const renderScreen = () => {
+        if (context) {
+
+          context.clearRect(0,0,10,10);
+
+          for (var playerId in game.players) {
+            const player = game.players[playerId];
+            context.fillStyle = 'black';
+            context.fillRect(player.x, player.y, 1, 1);
+          }
+
+          for (var fruitId in game.fruits) {
+            const fruit = game.fruits[fruitId];
+            context.fillStyle = 'green';
+            context.fillRect(fruit.x, fruit.y, 1, 1);
+          }
+        }
+      }
+      renderScreen();
+      
+
       const color = 'blue';
       const positionX = 50;
       const positionY = 50;
@@ -25,9 +59,11 @@ function Canvas({  }: CanvasProps) {
         context.fillRect(positionX, positionY, width, height);
       }
     }
-  }, [])
+
+  }, [context])
+
   return (
-    <canvas width={window.screen.width} height={window.screen.height} ref={canvasRef}/>
+    <CanvasContainer width="10" height="10" ref={canvasRef}/>
   );
 };
 
--- frontend/game-front/src/components/Tela/Canvas/styles.ts
+++ frontend/game-front/src/components/Tela/Canvas/styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
 export const CanvasContainer = styled.canvas`
-  
+    image-rendering: pixelated;
+    image-rendering: crisp-edges;
+    image-rendering: -moz-crisp-edges;
+
+    border: 10px solid gray;
+    width:400px;
+    height:400px;
 `;
