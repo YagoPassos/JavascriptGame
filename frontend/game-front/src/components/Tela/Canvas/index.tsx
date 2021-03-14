d337ab708f00bed0a6a4da1f9c8ac31f9510a230
YagoPassos <yp.anjos@hotmail.com>
2021-03-14 00:09:31 -0300

    setContext

--- frontend/game-front/src/components/Tela/Canvas/index.tsx
+++ frontend/game-front/src/components/Tela/Canvas/index.tsx
@@ -7,50 +7,51 @@ import { CanvasContainer } from './styles';
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    useEffect(() => {
+  const [context, setContext]: any = useState(null)
+  
+
+  useEffect(() => {
 
     if (canvasRef.current) {
-      const game : any={
+      const game: any = {
         players: {
           'Bob': { x: 1, y: 1 },
           'Paul': { x: 8, y: 8 }
         },
-    
+
         fruits: {
           'fruits': { x: 3, y: 1 }
         }
       }
-    
+
+
 
       const canvas = canvasRef.current;
-      const context = canvas.getContext('2d');
+      setContext(canvas.getContext('2d'));
 
+     
+
+      if (context){
       const renderScreen = () => {
-        if (context) {
-          
-          context.clearRect(0,0,10,10);
+            context.clearRect(0,0,10,10);
 
-          for (var playerId in game.players) {
-            const player = game.players[playerId];
-            context.fillStyle = 'black';
-            context.fillRect(player.x, player.y, 1, 1);
-          }
+            for (var playerId in game.players) {
+              const player = game.players[playerId];
+              context.fillStyle = 'black';
+              context.fillRect(player.x, player.y, 1, 1);
+            }
 
-          for (var fruitId in game.fruits) {
-            const fruit = game.fruits[fruitId];
-            context.fillStyle = 'green';
-            context.fillRect(fruit.x, fruit.y, 1, 1);
-            
-          }
+            for (var fruitId in game.fruits) {
+              const fruit = game.fruits[fruitId];
+              context.fillStyle = 'green';
+              context.fillRect(fruit.x, fruit.y, 1, 1);
+            }
+          requestAnimationFrame(renderScreen);
         }
-
-        requestAnimationFrame(renderScreen);
+        renderScreen();
       }
-      renderScreen();
     }
-    
-
-  }, [])
+  }, [context])
 
   return (
     <CanvasContainer width="10" height="10" ref={canvasRef} />
