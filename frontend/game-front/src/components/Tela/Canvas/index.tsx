77b07b684fd95b6bf05c8d881d33f4cd4cc662c4
YagoPassos <yp.anjos@hotmail.com>
2021-03-13 22:29:27 -0300

    ATT

--- frontend/game-front/src/components/Tela/Canvas/index.tsx
+++ frontend/game-front/src/components/Tela/Canvas/index.tsx
@@ -3,50 +3,52 @@ import { CanvasContainer } from './styles';
 
 /* eslint-disable */
 
+
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
- const [context, setContext] : any = useState([]);
- const [game, setGame] : any= useState([])
+    useEffect(() => {
 
-  useEffect(() => {
     if (canvasRef.current) {
-
-      setContext(canvasRef.current.getContext('2d'));
-
-      setGame( {
+      const game : any={
         players: {
           'Bob': { x: 1, y: 1 },
           'Paul': { x: 8, y: 8 }
         },
-
+    
         fruits: {
-          'fruits': { x: 3, y: 3 }
+          'fruits': { x: 3, y: 1 }
         }
-      })
+      }
+    
 
-    }
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
 
       const renderScreen = () => {
-      if (context) {   
-
-
+        if (context) {
+          
           context.clearRect(0,0,10,10);
 
           for (var playerId in game.players) {
-          const player = game.players[playerId];
-          context.fillStyle = 'black';
-          context.fillRect(player.x, player.y, 1, 1);
+            const player = game.players[playerId];
+            context.fillStyle = 'black';
+            context.fillRect(player.x, player.y, 1, 1);
+          }
+
+          for (var fruitId in game.fruits) {
+            const fruit = game.fruits[fruitId];
+            context.fillStyle = 'green';
+            context.fillRect(fruit.x, fruit.y, 1, 1);
+            
+          }
         }
 
-        for (var fruitId in game.fruits) {
-          const fruit = game.fruits[fruitId];
-          context.fillStyle = 'green';
-          context.fillRect(fruit.x, fruit.y, 1, 1);
-        }
+        requestAnimationFrame(renderScreen);
       }
+      renderScreen();
     }
-    renderScreen();
+    
 
   }, [])
 
