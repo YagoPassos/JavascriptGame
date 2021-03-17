a7c26abda6525c47e555a041121342165e39d75b
YagoPassos <yp.anjos@hotmail.com>
2021-03-16 22:32:24 -0300

    ATT interfaces

--- frontend/game-front/src/components/Tela/Canvas/index.tsx
+++ frontend/game-front/src/components/Tela/Canvas/index.tsx
@@ -3,17 +3,45 @@ import { CanvasContainer } from './styles';
 
 /* eslint-disable */
 
+interface Command {
+  playerId: string;
+  keyPressed: any;
+}
+
+interface Coordinates{
+  x: number;
+  y:  number;
+}
+
+interface Players{
+  'Bob' : Coordinates;
+  'Paul': Coordinates;
+}
+
+interface Fruits{
+  fruits : Coordinates;
+}
+
+interface State{
+  players: Players;
+  fruits: Fruits;
+}
+
 
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const [context, setContext]: any = useState(null)
-  
-
-  useEffect(() => {
+    useEffect(() => {
 
     if (canvasRef.current) {
-      const game: any = {
+     
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+      const currentPlayerId = 'Bob';
+
+     const createGame =() =>{
+
+      const state : State = {
         players: {
           'Bob': { x: 1, y: 1 },
           'Paul': { x: 8, y: 8 }
@@ -24,25 +52,64 @@ function Canvas() {
         }
       }
 
+       function movePlayer(command: Command){
+          console.log(`Moving ${command.playerId} with ${command.keyPressed}`)
 
+          const keyPressed = command.keyPressed;
+          const player = game.state.players[command.playerId]
 
-      const canvas = canvasRef.current;
-      setContext(canvas.getContext('2d'));
+          if (keyPressed === 'ArrowUp' && player.y - 1 >= 0){
+            
+          }
+          if (keyPressed === 'ArrowRight' && player.x - 1 < 10){
 
-     
+          }
+          if (keyPressed === 'ArrowDown' && player.x - 1 < 10){
+
+          }
+          if (keyPressed === 'ArrowLeft' && player.y - 1 >= 0){
+
+          }
+       }
+
+       return{
+         movePlayer,
+         state
+       }
+     }
+
+    
+
+     const handleKeydown = (event : any) => {
+        const keyPressed = event.key;
+        
+        
+        const command : Command = {
+            playerId: 'Bob',
+            keyPressed: keyPressed
+        }
+
+          game.movePlayer(command)
+     }
+
+     document.addEventListener('keydown', handleKeydown)
+        
+      const game = createGame();
+      
+   
 
       if (context){
       const renderScreen = () => {
             context.clearRect(0,0,10,10);
 
-            for (const playerId in game.players) {
-              const player = game.players[playerId];
+            for (const playerId in game.state.players) {
+              const player = game.state.players[playerId];
               context.fillStyle = 'black';
               context.fillRect(player.x, player.y, 1, 1);
             }
 
-            for (const fruitId in game.fruits) {
-              const fruit = game.fruits[fruitId];
+            for (const fruitId in state.fruits) {
+              const fruit = game.state.fruits[fruitId];
               context.fillStyle = 'green';
               context.fillRect(fruit.x, fruit.y, 1, 1);
             }
@@ -52,7 +119,7 @@ function Canvas() {
         renderScreen();
       }
     }
-  }, [context])
+  }, [])
 
   return (
     <CanvasContainer width="10" height="10" ref={canvasRef} />
